bcff1d36cfc2a2bc69bdc79e473d77cecb56c480
huang825172 <1048035187@qq.com>
2020-12-01 21:40:46 +0800

    Update vue.config.js

--- web/vue.config.js
+++ web/vue.config.js
@@ -11,7 +11,8 @@ module.exports = {
         port: 8081,
         proxy: {
             '/api': {
-                target: "http://119.29.60.125:3000",
+                // 更改为部署服务器地址
+                target: "http://119.29.60.125",
                 changeOrigin: true,
             },
         },
