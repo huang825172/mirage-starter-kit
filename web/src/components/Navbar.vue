c7e3c71a9ddefe056bec2fcfefb60c8df1b593ee
huang825172 <1048035187@qq.com>
2020-12-02 10:49:17 +0800

    Add: HTTP triggered deploy

--- web/src/components/Navbar.vue
+++ web/src/components/Navbar.vue
@@ -2,6 +2,7 @@
   <div>
     <b-navbar type="light" variant="light" id="nav">
       <b-navbar-nav class="ml-auto">
+        <b-nav-item href="https://github.com/huang825172/mirage-starter-kit">Github 仓库</b-nav-item>
         <b-nav-item href="/">首页</b-nav-item>
         <b-nav-item href="/NoPage">404</b-nav-item>
         <b-nav-item href="/about">关于</b-nav-item>
